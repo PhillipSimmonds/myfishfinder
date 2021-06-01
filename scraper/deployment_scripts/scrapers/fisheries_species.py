ff44a7ab29357cc52d1d488ef688b33ad0b5a98b
Phillip Simmonds <phillipsimmonds96@gmail.com>
2021-06-01 22:22:48 +1200

    added comments

--- scraper/deployment_scripts/scrapers/fisheries_species.py
+++ scraper/deployment_scripts/scrapers/fisheries_species.py
@@ -6,6 +6,8 @@ import datetime as dt
 
 
 def run_fisheries_collector():
+    # Output: 2016 - Current species fisheries data
+    # Set the columns for the dataframe
     columns = [
         "SpeciesCode",
         "Name",
@@ -18,12 +20,15 @@ def run_fisheries_collector():
         "InQMS",
         "Year"
     ]
+    # Get the current year to run though - back to 2016
     year = dt.datetime.now().year
     data = []
     for i in range(2016,year+1):
         rq = req.get("https://fs.fish.govt.nz/Page.aspx?pk=6&tk=97&ey={}".format(i))
         html = BeautifulSoup(rq.content)
         for row in html.select("tr.ItemRow"):
+            # Includes year
             data.append([d.text.replace("\n","").replace(",","") for d in row.select("td")] + [i])
     df = pd.DataFrame(data=data,columns=columns)
+    # Save ouput
     df.to_csv("scraper/data/raw/fisheries/species/{}-{}-{}.csv".format(dt.datetime.now().year,dt.datetime.now().month,dt.datetime.now().day))
